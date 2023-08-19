aa796e51c6543be591ad1425518c28ea86c9cc1e
ch-yang1273 <ych1273@gmail.com>
2023-08-19 18:59:42 +0900

    Initial commit

---
+++ src/main/java/community/whatever/petcoming/PetcomingApplication.java
@@ -0,0 +1,13 @@
+package community.whatever.petcoming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcomingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcomingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/community/whatever/petcoming/PetcomingApplicationTests.java
@@ -0,0 +1,13 @@
+package community.whatever.petcoming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcomingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
