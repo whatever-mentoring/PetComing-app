02bf7f04080b2de56a31eddcf05671b73647d422
ch-yang1273 <ych1273@gmail.com>
2023-09-03 21:09:38 +0900

    ci: CodeDeploy 설정 파일 작성 (실행 스크립트)

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+BUILD_JAR=$(ls /home/ec2-user/build/build/libs/petcoming-0.0.1-SNAPSHOT.jar)
+JAR_NAME=$(basename $BUILD_JAR)
+echo "> build file name: $JAR_NAME" >> /home/ec2-user/deploy.log
+
+echo "> Checl the currently running application PID" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> No applications are currently running" >> /home/ec2-user/deploy.log
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> build file copy" >> /home/ec2-user/deploy.log
+DEPLOY_PATH=/home/ec2-user/
+cp $BUILD_JAR $DEPLOY_PATH
+
+DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
+PROPERTIES_LIST=file:/home/ec2-user/build/src/main/resources/application.yml
+echo "> DEPLOY_JAR deploy" >> /home/ec2-user/deploy.log
+echo "> properties: $PROPERTIES_LIST" >> /home/ec2-user/deploy.log
+nohup java -jar -Dspring.config.location=$PROPERTIES_LIST $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
