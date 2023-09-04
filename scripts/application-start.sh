45d1c326073c486bd0ffb28ed05c19ae515171b9
ch-yang1273 <ych1273@gmail.com>
2023-09-04 23:49:15 +0900

    ci: application.yml 설정을 운영, 개발로 분리 및 MariaDB 활성화

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -21,7 +21,7 @@ DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-PROPERTIES_LIST=file:/home/ec2-user/build/src/main/resources/application.yml
+PROPERTIES_LIST=file:/home/ec2-user/resources/application.yml,file:/home/ec2-user/resources/application-prod.yml
 echo "> DEPLOY_JAR deploy" >> /home/ec2-user/deploy.log
 echo "> properties: $PROPERTIES_LIST" >> /home/ec2-user/deploy.log
 nohup java -jar -Dspring.config.location=$PROPERTIES_LIST $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/deploy_err.log &
