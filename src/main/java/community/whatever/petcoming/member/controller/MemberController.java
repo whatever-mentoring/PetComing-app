e6a91bbb50a60d6b3b6f82589bd4eb8568b01d4d
ch-yang1273 <ych1273@gmail.com>
2023-09-06 21:51:09 +0900

    fix: 로그인하지 않아도 AUTHORIZED를 반환하는 버그 수정

--- src/main/java/community/whatever/petcoming/member/controller/MemberController.java
+++ src/main/java/community/whatever/petcoming/member/controller/MemberController.java
@@ -5,8 +5,9 @@ import community.whatever.petcoming.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +22,12 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/auth-check")
-    public ResponseEntity<String> authCheck(@AuthenticationPrincipal UserDetails userDetails) {
-        if (userDetails != null) {
-            return ResponseEntity.ok().body("AUTHORIZED");
-        } else {
+    public ResponseEntity<String> authCheck() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication.getPrincipal().equals("anonymousUser")) {
             return ResponseEntity.ok().body("UNAUTHORIZED");
+        } else {
+            return ResponseEntity.ok().body("AUTHORIZED");
         }
     }
 
